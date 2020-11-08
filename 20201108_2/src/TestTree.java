315a207660f3eacdc817dc22fe42f5a6ae37571d
ZShuangShuang <1434278632@qq.com>
2020-11-08 22:15:14 +0800

    提交代码

---
+++ 20201108_2/src/TestTree.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 14342
+ * Date: 2020-11-08
+ * Time: 20:23
+ **/
+public class TestTree {
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+
+    }
+}
