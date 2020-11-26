d62f8f705a28f9132796f8e7521d32031240fa0b
ZShuangShuang <1434278632@qq.com>
2020-11-26 18:25:58 +0800

    提交代码

---
+++ DailyTest_20201126/src/Main.java
@@ -0,0 +1,94 @@
+import java.util.Scanner;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 14342
+ * Date: 2020-11-26
+ * Time: 17:25
+ **/
+public class Main {
+    private float f = 1.0f;
+    int m = 12;
+    static int n =1;
+
+    public static void main(String[] args) {
+       /* short a =128;
+
+        byte b =(byte) a;
+        System.out.println(a);
+        System.out.println(b);*/
+        /*double x = 2.0;
+        int y = 4;
+        System.out.println(x /= ++y);*/
+        Main main = new Main();
+        
+    }
+}
+    /*public static int func(int val){
+        if (val < 0){
+            return 1;
+        }else if (val == 0){
+            return 0;
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNext()){
+            String s = scanner.nextLine();
+            int val = Integer.parseInt(s);
+            System.out.println(func(val));
+        }
+    }
+}*/
+   /* public static void isModByFive(int x){
+        if (x%5 == 0){
+            System.out.println("YES");
+        }else {
+            System.out.println("NO");
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNext()){
+            String str = scanner.nextLine();
+            int val = Integer.parseInt(str);
+            isModByFive(val);
+        }
+    }
+}*/
+    /*public static int cal(String s){
+        String[] ret = s.split(" ");
+        int a = Integer.parseInt(ret[0]);
+        int b = Integer.parseInt(ret[1]);
+        int c = Integer.parseInt(ret[2]);
+        int d = Integer.parseInt(ret[3]);
+        return (a+b-c)*d;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        System.out.println(cal(s));
+    }
+}*/
+  /*  public static void main(String[] args) {
+        System.out.println("I will learn C++ well!");
+    }
+}*/
+   /* //判断是否能成功登录
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            String str = sc.nextLine();
+            String[] ret = str.split(" ");
+            if ("admin".equals(ret[0]) && "admin".equals(ret[1])){
+                System.out.println("Login Success!");
+            }else {
+                System.out.println("Login Fail!");
+            }
+        }
+    }
+}
+*/
